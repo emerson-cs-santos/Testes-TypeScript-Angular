41234d8a49f551b4462d2805560e9cf900b88120
Emerson Costa <syphonemerson@yahoo.com.br>
2020-03-03 22:04:40 -0300

    Aula 3 parte 2

--- Angular/app1/src/app/app.module.ts
+++ Angular/app1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DateComponent } from './views/date/date.component';
+import { SobreComponent } from './views/sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
---
+++ Angular/app1/src/app/views/sobre/sobre.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  templateUrl: './sobre.component.html',
+  styleUrls: ['./sobre.component.css']
+})
+export class SobreComponent implements OnInit {
+
+  sobre: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.sobre = 'Sobre essa aplicação';
+  }
+
+}
